3dd417f804aedf4d592bad59adda0faa1f565a0a
unknown <grro@LWKA-JH5WKX1.united.domain>
2015-02-12 18:29:14 +0100

    code optimized

--- reactive-http/src/main/java/net/oneandone/reactive/rest/container/ResultConsumer.java
+++ reactive-http/src/main/java/net/oneandone/reactive/rest/container/ResultConsumer.java
@@ -39,12 +39,7 @@ public class ResultConsumer implements BiConsumer<Object, Throwable> {
     
     @Override
     public void accept(Object result, Throwable error) {
-        
-        if (error == null) {
-            asyncResponse.resume(result);            
-        } else {
-            asyncResponse.resume(unwrapIfNecessary(error, 10));
-        }
+        asyncResponse.resume((error == null) ? result : unwrapIfNecessary(error, 10));            
     }
     
     
